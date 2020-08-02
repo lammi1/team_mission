b91532b66287507a3e4d3fa2fe298c4c3451a655
lammi1 <67054161+lammi1@users.noreply.github.com>
2020-08-02 16:45:33 +0900

    Create 4_sample_bubble.c

---
+++ 4_sample_bubble.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int number[5] = {1,2,7,9,1};
+    int temp;
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5 - i - 1; j++)
+        {
+            if (number[j] > number[j+1])
+            {
+                temp = number[j];
+                number[j] = number[j+1];
+                number[j+1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%i", number[i]);
+    }
+    printf("\n");
+}
